c4a084b0b96144954775d0fe82ae111c3af41652
Shashank Srivatsa Kuchibhotla <sk.kuchibhotla@gmail.com>
2020-10-17 16:11:52 -0700

    Adding WaterJugProblem

---
+++ WaterJugProblem/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace WaterJugProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter m: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter d: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            Program p = new Program();
+            p.FillJug(m, n, d);
+
+            Console.ReadKey();
+
+        }
+
+        public void FillJug(int m, int n, int d)
+        {
+            int jug1 = 0;
+            int jug2 = 0;
+
+            while(jug1 != d || jug2 != d)
+            {
+                // Fill jug1
+                if (jug1 == 0) 
+                { 
+                    jug1 = 3;
+                    Console.WriteLine(jug1 + "," + jug2);
+                }
+
+                // Fill jug2 if jug1 is full. Empty jug1.
+                if(jug1 == m) 
+                {
+                    if(jug2 == 0)
+                    {
+                        jug2 = jug1;
+                        jug1 = 0;
+                        Console.WriteLine(jug1 + "," + jug2);
+                    }
+                    if(jug2 != 0)
+                    {
+                        jug2 += jug1;
+                        jug1 = 0;
+                        Console.WriteLine(jug1 + "," + jug2);
+                    }
+                }
+
+                // Empty jug2 if full.
+                if(jug2 == n)
+                {
+                    jug2 = 0;
+                    Console.WriteLine(jug1 + "," + jug2);
+                }
+            }
+        }
+    }
+}
