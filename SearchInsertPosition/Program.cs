e14391516b20e13a65a6e262308f58f74fa31f54
Shashank Srivatsa Kuchibhotla <sk.kuchibhotla@gmail.com>
2020-10-18 14:44:51 -0700

    Adding Search Index Position problem solution

---
+++ SearchInsertPosition/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Dynamic;
+
+namespace SearchInsertPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 3, 6, 5 };
+            int target = 5;
+
+            Program obj = new Program();
+            int result = obj.SearchInsert(nums, target);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            int i;
+            for (i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < target) { continue; }
+                else if (nums[i] >= target) { return i; }
+            }
+
+            return i;
+        }
+    }
+}
